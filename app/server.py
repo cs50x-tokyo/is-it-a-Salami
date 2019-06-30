b234e672141eb3b107cf156a7199ce89dcc02b91
T.Salami <salami.tope@gmail.com>
2019-06-30 16:56:41 +0900

    Update server.py

--- app/server.py
+++ app/server.py
@@ -62,11 +62,12 @@ async def analyze(request):
     img = open_image(BytesIO(img_bytes))
     prediction, prediction_id, prediction_probability = learn.predict(img)[0]
     #return JSONResponse({'result': str(prediction)})
-    return JSONResponse({
-        'result': str(prediction), 
-        'probabilities': str(prediction_probability)
-       })
-    #return JSONResponse({str(prediction_probabilty) 'that this is a 'result': str(prediction)})
+    
+    #return JSONResponse({
+    #    'result': str(prediction), 
+    #    'probabilities': str(prediction_probability)
+    #   })
+    return JSONResponse({str(prediction_probabilty) 'that this is a 'result': str(prediction)})
 
 
 if __name__ == '__main__':
